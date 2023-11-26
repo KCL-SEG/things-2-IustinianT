11112c26c59435a34f0223175c385f83e8cbdb94
IustinianT <iusti2003@yahoo.com>
2023-11-26 18:37:11 +0000

    Create ThingForm

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,14 @@
 """Forms of the project."""
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Thing
 
 # Create your forms here.
+class ThingForm(forms.ModelForm):
+
+    class Meta:
+        model = Thing
+        fields = ["name", "description", "quantity"]
+    
+    def is_valid(self):
+        return super().is_valid()
